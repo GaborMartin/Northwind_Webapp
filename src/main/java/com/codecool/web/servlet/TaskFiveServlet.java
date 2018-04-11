79a769e1f86c74cf25230ea0cc60718a94944eb8
GaborMartin <gabormartin94@gmail.com>
2018-04-11 12:32:10 +0200

    Implemented servlets for tasks

---
+++ src/main/java/com/codecool/web/servlet/TaskFiveServlet.java
@@ -0,0 +1,32 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.dao.TaskDao;
+import com.codecool.web.dao.database.DatabaseTaskDao;
+import com.codecool.web.model.Model;
+import com.codecool.web.service.TaskService;
+import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimpleTaskService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TaskFiveServlet extends AbstractServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao);
+            List<Model> result = taskService.getResulOfTaskFive();
+            req.setAttribute("result", result);
+        } catch (SQLException | ServiceException e) {
+            req.setAttribute("error", e.getMessage());
+        }
+        req.getRequestDispatcher("task5.jsp").forward(req,resp);
+    }
+}
+
