61dac667bb50777c31f4a02d73af629582f4e46d
GaborMartin <gabormartin94@gmail.com>
2018-04-11 13:43:56 +0200

    Modified annotations

--- src/main/java/com/codecool/web/servlet/TaskFiveServlet.java
+++ src/main/java/com/codecool/web/servlet/TaskFiveServlet.java
@@ -8,13 +8,14 @@ import com.codecool.web.service.exception.ServiceException;
 import com.codecool.web.service.simple.SimpleTaskService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
-
+@WebServlet("/task5")
 public class TaskFiveServlet extends AbstractServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
